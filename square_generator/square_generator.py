88bd0ce987d07163e14457ad63abfe21475a9287
s28305 <s28305@pjwstk.edu.pl>
2024-03-15 20:16:40 +0100

    Task 9

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -22,3 +22,26 @@ print("Squares:", square_x.square_list)
 
 cubic_x = CubicGenerator(1, 11)
 print("Cubes:", cubic_x.cube_list)
+
+
+# Task9: FunctionOverriding
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if start <= end:
+            return [n ** 2 for n in range(start, end)]
+        else:
+            raise ValueError("Invalid values inserted")
+
+
+class CubicGenerator(SquareGenerator):
+    def generate_cubes(self, start, end):
+        if start <= end:
+            return [n ** 3 for n in range(start, end)]
+        else:
+            raise ValueError("Invalid values inserted")
+
+    def generate_squares(self, start, end):
+        if start <= end:
+            return [n ** 2 for n in range(start, end)]
+        else:
+            raise ValueError("Start value needs to be less or equal to the end")
