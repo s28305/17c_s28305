e847e0d1ce747782f0726bf249d52c8e7bb0e775
s28305 <s28305@pjwstk.edu.pl>
2024-03-15 13:22:14 +0100

    Task 2

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # Task1:ListComprehensions
-squareList = [n**2 for n in range(1, 11)]
-print(squareList)
+square_list = [n**2 for n in range(1, 11)]
+print(square_list)
+
+
+# Task2: Functions
+def square_in_range(i, j):
+    return [n ** 2 for n in range(i, j)]
+
+
+print(square_in_range(1, 15))
+
