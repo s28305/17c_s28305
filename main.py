387caa1b6a48451742db0bcc2c5619c437343c74
s28305 <s28305@pjwstk.edu.pl>
2024-03-15 13:31:47 +0100

    Task 3

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ print(square_list)
 
 
 # Task2: Functions
-def square_in_range(i, j):
-    return [n ** 2 for n in range(i, j)]
+def e_squares(start, end):
+    return [n ** 2 for n in range(start, end)]
 
 
-print(square_in_range(1, 15))
+print(e_squares(1, 15))
 
+
+# Task3: Classes
+class SquareGenerator:
+    def __init__(self, start, end):
+        self.square_list = [n ** 2 for n in range(start, end)]
+
+
+x = SquareGenerator(1, 15)
+print(x.square_list)
